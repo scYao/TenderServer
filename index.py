80970ef010699faf00e28ccf29bf9e070edf7212
scYao <2247523672@qq.com>
2018-04-08 02:05:14 +0800

    none

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ from user.user_manager import UserManager
 from flask import request
 import json
 
-@app.route('/add_user', methods=['GET', 'POST'])
+@app.route('/add_user/', methods=['GET', 'POST'])
 def add_user():
     userManager = UserManager()
     data = {}
@@ -16,4 +16,18 @@ def add_user():
             data['status'] = 'success'
             data['data'] ='you has add user successfully'
         return json.dumps(data)
+
+
+@app.route('/login/', methods=['GET'])
+def login():
+    userManager = UserManager()
+    data = {}
+    data['status'] = 'failed'
+    data['data'] = ''
+    if request.method == 'GET':
+        paramsJson  = request.form['data']
+        (status, result) = userManager.login(paramsJson)
+
+        if status is True:
+            pass
 
